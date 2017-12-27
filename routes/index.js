1cce5802a3fddf56d790f8b4e318efdd4889e05b
Rudi Starcevic <ooly.me@gmail.com>
2017-12-27 17:05:21 +0800

    Debug metrics counter by name

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const metrics = new Lynx('telegraf', 8125)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  metrics.increment('servicez.home')
+  metrics.increment('service.home')
   res.render('index', { title: 'Viseo Hong Kong - Digital Innovation Labs', hostname: os.hostname, timestamp: new Date().toLocaleString() });
 });
 
