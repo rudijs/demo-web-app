ff3a8304e3c2f8604812784fef1f5ac083df49aa
Rudi Starcevic <ooly.me@gmail.com>
2017-12-27 16:34:55 +0800

    Add lynx, remove prom-client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const os = require('os')
+const Lynx = require('lynx')
+
+const metrics = new Lynx('telegraf', 8125)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  metrics.increment('service.main')
   res.render('index', { title: 'Viseo Hong Kong - Digital Innovation Labs', hostname: os.hostname, timestamp: new Date().toLocaleString() });
 });
 
